6d65e38f79ef98fcd1bade304470e0d500bee630
phiaseitz <sophia.seitz@students.olin.edu>
2014-10-02 22:33:19 -0400

    Implemented a simple likelihood function

--- crimeshow.py
+++ crimeshow.py
@@ -15,26 +15,75 @@ class Crime(thinkbayes2.Suite):
         data: time, characters implicated, how strongly
         """
         #Assuming that for this case, the hypothesis is correct
-        time, character, howstrong = data
-        
-        if hypo == character:
-        	like = 1
-        else:
-        	like = 0.1
+        time = data[0]
+        characterTuples = data [1:]
+        print (characterTuples)
+         
+        for charTup in characterTuples:
+            if hypo == charTup[0]:
+            	like = charTup[1] * time/10
+                return like
+            else:
+                like=0.5*time/10
         return like
    
 
 
 def main():
-    hypos = numpy.linspace(0, 10, 11)
+    hypos = numpy.linspace(0, 6, 7)
     suite = Crime(hypos)
 
     #thinkplot.Hist(suite, label='prior')
 
-    data = 1, 3, 5
+    data = 1, (6,0.8)
     suite.Update(data)
-    data = 1, 10, 4
+
+    data = 2, (6,0.8)
     suite.Update(data)
+
+    data = 3, (1,0.8)
+    suite.Update(data)
+
+    data = 4, (2,0.8)
+    suite.Update(data)
+
+    data = 5, (2,0.8)
+    suite.Update(data)
+
+    data = 6, (2,0.9)
+    suite.Update(data)
+
+    data = 7, (0,0.8), (1,0.8)
+    suite.Update(data)
+
+    data = 8, (0,0.4)
+    suite.Update(data)
+
+    data = 9, (1,0.8), (2, 0.8)
+    suite.Update(data)
+    """
+    data = 10, (2,0.8)
+    suite.Update(data)
+
+    data = 11, (2,0.8)
+    suite.Update(data)
+
+    data = 12, (2,0.1)
+    suite.Update(data)    
+
+    data = 13, (0,0.8), (1,0.8), (2,0.1)
+    suite.Update(data)
+
+    data = 14, (1,0.8)
+    suite.Update(data)
+
+    data = 15, (0,0.9)
+    suite.Update(data)
+
+    data = 16, (0,0.8)
+    suite.Update(data)"""
+
+
 
     thinkplot.Hist(suite, label='posterior')
     thinkplot.Show()
