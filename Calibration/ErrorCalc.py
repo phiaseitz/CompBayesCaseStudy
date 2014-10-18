6369f48282e74e363cc5bc942c9f7e6b46972b48
phiaseitz <sophia.seitz@students.olin.edu>
2014-10-18 12:12:59 -0400

    Super commented code

--- Calibration/ErrorCalc.py
+++ Calibration/ErrorCalc.py
@@ -1,13 +1,4 @@
-"""This file contains code for use with "Think Bayes",
-by Allen B. Downey, available from greenteapress.com
-
-Copyright 2014 Allen B. Downey
-License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
-"""
-
 from __future__ import print_function, division
-
-import numpy
 from pylab import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,81 +7,59 @@ import time
 from itertools import product
 
 
-def getSweepData(realDist,numdata):
+def getSweepData(realDist,numReadings):
     #For Linux (I think)
     #ser=serial.Serial('/dev/ttyACM0', 9600) #Defines the serial port to use 
     #For Mac
     ser=serial.Serial('/dev/tty.usbmodem1411', 9600) #Defines the serial port to use 
     time.sleep(1)
-
-    ser.write('4') #Sets the servo over to its home position on the right
-    time.sleep(0.5) #Waits half a second to ensure the arduino is ready for next command
-    readings = []
-
-    horzAngle=range(0, angleRange+1) #creates the horizontal range of angles that the servo should sweep through
-
-    for eachHorizontalAngle in horzAngle:
-        #print ('getting reading')
-        ser.write('5') #Tells the arduino to send back a distance reading
+    #Sets the servo over to its home position on the right
+    ser.write('4') 
+    #Waits half a second to ensure the arduino is ready for next command
+    time.sleep(0.5) 
+    #Set up a list for the readings
+    errors = []
+    #Get a reading as many times as we want to 
+    for eachReading in range(numReadings):
+        #Tells the arduino to send back a distance reading
+        ser.write('5') 
+        #Wait to make sure that the arduino is ready
         time.sleep(0.3)
         try:
-            distance_response=ser.readline() #Receives the distance reading from the arduino
-            cleanReading=distance_response[1:-2] #Removes the last 2 characters ("\r\n") from the arduino output
-            #distance=float(cleanReading) #Type-casts it as an float so that it can be plotted
-            print (cleanReading)
-            distance = arduinoOutputToDistance(int(cleanReading))
-            print (distance)
-            err = distance - realDist
-            readings.append(err)  
-            print (eachHorizontalAngle)          
+            #Receives the distance reading from the arduino
+            distance_response=ser.readline() 
+            #Removes the last 2 characters ("\r\n") from the arduino output
+            cleanReading=distance_response[1:-2] 
+            #Calculate the distance based on the reading
+            distance = arduinoOutputToDistance(int(cleanReading)) 
+            #Calculate error of measured distance from the known distance
+            err = distance - realDist 
+            #Add the error to our list of errors
+            errors.append(err)  
+            #print (eachReading)          
         except Exception, e:
-            print ('did not work' +  str(eachHorizontalAngle))
+            print ('did not work' +  str(eachReading))
         else:
             pass
         finally:
             pass
-    return readings
+    return errors  
 
  
 def arduinoOutputToDistance(output):
+    #Find the distance based on the output of the arudino (a number 0-1023)
     outputVoltage = output*5.0/1023
     distance = 61.173*outputVoltage**(-1.159)
     return distance
 
 def main():
+    #Get readings from the arduino
     err = getSweepData(120,90)
-    print (err)
+    #print (err)
     mean = np.mean(err)
     print (mean)
     std = np.std(err)
     print (std)
-    # rs = np.linspace(20,150,50)
-    # thetas = np.arange(0,90)
-    # joint = Lidar(product(rs, thetas))
-
     
-
-
-
-    # data = ((15, 10),(16,10))
-
-    # joint.Update(data)
-    
-    # # thinkplot.Contour(joint.GetDict(), contour=False, pcolor=True)
-    # # thinkplot.Show(xlabel='X', ylabel='Y')
-
-    # data = ((15,12),(16,12))
-
-    # joint.Update(data)
-
-
-    # plotPolar(joint)
-
-    # # thinkplot.Contour(joint.GetDict(), contour=False, pcolor=True)
-    # # thinkplot.Show(xlabel='X', ylabel='Y')
-
-    # # TODO: plot the marginals and print the posterior means
-
-
 if __name__ == '__main__':
     main()
--- Lidar.py
+++ Lidar.py
@@ -8,13 +8,14 @@ import serial
 import time
 from itertools import product
 
+#Defining variables up here so that we make our lives easier
 maxAngle = 80
 minRange = 20
 maxRange = 150
 numPoints = 50
 
 class Lidar(thinkbayes2.Suite):
-    """Represents hypotheses about the location of objects sensor is at 0,0."""
+    """Represents hypotheses about the location of objects (sensor is at 0,0)."""
     def __init__(self, hypos):
         self.mean = 0
         self.std = 0
@@ -32,12 +33,13 @@ class Lidar(thinkbayes2.Suite):
         hypo: hypothetical distance
         data: measured distance.
         """
-
+        #Calculate the likelihood that if we were at distance =hypo that we would get a reading = data
         error = data - hypo
         #Mean and STD calculated based on distance 
         mean = 0.0054*hypo**2 - 0.4089*hypo + 6.8076
         std = 0.1211*hypo - 3.2346
 
+        #The error is normally distributed
         like = thinkbayes2.EvalNormalPdf(error, mean, std)
         if math.isnan(like):
             like = 5e-50
@@ -46,6 +48,8 @@ class Lidar(thinkbayes2.Suite):
         return like
 
 def UpdateSuites(suites, data):
+    #We take in an array of all the angle,reading pairs and our suites
+    #for each of the measurements we look at the angle and update the appropriate suite
     for measurement in data:
         r = measurement[0]
         theta = measurement[1]
@@ -54,33 +58,24 @@ def UpdateSuites(suites, data):
 
 
 def plotPolar(suites):
+    #Create arrays of r, theta, and theta converted to radians
     rsarray = np.linspace(minRange,maxRange,numPoints)
     thetasarray = np.arange(0,maxAngle)
     thetasradarray = np.radians(thetasarray)
 
+    #Meshgrid for thinkplot
     rs,thetasrads = np.meshgrid(thetasradarray, rsarray)
     likes = np.zeros((rsarray.size,thetasarray.size))
-    # print (rsarray)
-    # for theta,suite in enumerate(suites):
-    #     for hypo, prob in suite.Items():
-    #         likes[np.where(rsarray == hypo)][theta] = prob
-    # print (likeDicts)
-
+    
+    #Find the likelihood of a given r,theta pair so that we can color the contour plot appropriately
     for theta in thetasarray:
         currentSuite = suites[theta]
         for indexr,r in enumerate(rsarray):
-            #print(currentSuite.Prob(r))
+            
             likes[indexr][theta] = currentSuite.Prob(r)
-    
-    # for indextheta,theta in enumerate(thetasarray):
-    #     likeDict = likeDicts[theta]
-    #     print (likeDict)
-    #     for indexr,r in enumerate(rsarray):
-    #         print (likeDict[r])
-    #         #likes[indexr][indextheta] = likeDict[r]
+   
 
-
-
+    #Plotting things
     fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
     ax.contourf(rs,thetasrads, likes)
 
@@ -93,25 +88,33 @@ def getSweepData(angleRange):
     ser=serial.Serial('/dev/tty.usbmodem1411', 9600) #Defines the serial port to use 
     time.sleep(1)
 
-    ser.write('4') #Sets the servo over to its home position on the right
-    time.sleep(0.5) #Waits half a second to ensure the arduino is ready for next command
+    #Sets the servo over to its home position on the right
+    ser.write('4') 
+    #Waits half a second to ensure the arduino is ready for next command
+    time.sleep(0.5) 
+    #Create a lits of readings
     readings = []
+    #Our range of angles to run through
+    Angle=range(0, angleRange) 
 
-    horzAngle=range(0, angleRange) #(so we don't fight the servo) creates the horizontal range of angles that the servo should sweep through
-
-    for eachHorizontalAngle in horzAngle:
+    for eachHorizontalAngle in Angle:
         #print ('getting reading')
-        ser.write('5') #Tells the arduino to send back a distance reading
+        #Tells the arduino to send back a distance reading
+        ser.write('5') 
         time.sleep(0.3)
         try:
-            distance_response=ser.readline() #Receives the distance reading from the arduino
-            cleanReading=distance_response[2:-2] #Removes the last 2 characters ("\r\n") from the arduino output
-            #distance=float(cleanReading) #Type-casts it as an float so that it can be plotted
-            #print ('clean' + str(cleanReading))
+            #Receives the distance reading from the arduino
+            distance_response=ser.readline() 
+            #Removes the last 2 characters ("\r\n") from the arduino output
+            cleanReading=distance_response[2:-2] 
+            #Calculate distance from arduino reading
             distance = arduinoOutputToDistance(int(cleanReading))
-            print (eachHorizontalAngle)
+            #print (eachHorizontalAngle)
+            #If we're given NaN - set the distance to the maximum distance we can meausre at
+            #This is not the world's best fix
             if math.isnan(distance):
                 distance = maxRange
+            #Add each reading to our list of readings
             readings.append((distance, eachHorizontalAngle))
         except Exception, e:
             print ('did not work' +  str(eachHorizontalAngle))
@@ -124,31 +127,39 @@ def getSweepData(angleRange):
         
         
         #print('Moving left')
-        ser.write('2') #Moves the servo one degree to the left after taking a reading
+        #Moves the servo one degree to the left after taking a reading
+        ser.write('2') 
         time.sleep(0.3)
-    time.sleep(0.5)
-    ser.write('4') #reset servo
+    #reset servo
+    ser.write('4') 
     return readings
    
  
 def arduinoOutputToDistance(output):
+    #calculate distance based on arduino output
     outputVoltage = output*5.0/1023
     distance = 32.336*outputVoltage**(-0.845)
     return distance
 
 def main():
+    #All the rs
     rs = np.linspace(minRange,maxRange,numPoints)
+    #all the thetas
     thetas = np.arange(0,maxAngle)
+    #A list of what will be all our suites
     suites = []
 
+    #Create one squite per theta and add it to our list of suites
     for theta in thetas:
         suites.append(Lidar(rs))
 
+    #How many times do we want to collect data and update
     numRuns = 5
+    #Collect data and update that many times
     for run in range(numRuns):
         data = getSweepData(maxAngle)
-    #print (data)
         UpdateSuites(suites, data)
+        #Plot the state of our hypotheses after each update
         plotPolar(suites)
 
     
